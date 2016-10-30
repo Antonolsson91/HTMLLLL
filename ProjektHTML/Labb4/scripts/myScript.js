acf5d846714ec6e341b74bbf3c87ac8a2e1d5a4f
Antonolsson91 <antonolsson91@yahoo.com>
2016-10-30 07:19:29 +0100

    början till spelet

---
+++ ProjektHTML/Labb4/scripts/myScript.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+    //console.log("ready!"); 
+    draw();
+});
+
+function draw() {
+    var canvas = document.getElementById('Canvas1');
+    var ctx = canvas.getContext('2d')
+
+
+
+    var my_gradient = ctx.createLinearGradient(10, 10, 10, 400);
+    my_gradient.addColorStop(0, "white");
+    my_gradient.addColorStop(0.5, "blue");
+    my_gradient.addColorStop(1, "brown");
+    ctx.fillStyle = my_gradient;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+
+}
