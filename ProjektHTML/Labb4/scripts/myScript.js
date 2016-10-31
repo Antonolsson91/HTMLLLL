c2bdffd8979f8c6336383162684a84876e5b575e
Antonolsson91 <antonolsson91@yahoo.com>
2016-10-31 01:03:06 +0100

    Klar med index, galleri, video och spelet

--- ProjektHTML/Labb4/scripts/myScript.js
+++ ProjektHTML/Labb4/scripts/myScript.js
@@ -1,20 +1,8 @@
-﻿$(document).ready(function () {
-    //console.log("ready!"); 
-    draw();
-});
-
-function draw() {
-    var canvas = document.getElementById('myCanvas');
-    var ctx = canvas.getContext('2d')
-
-
-
-    var my_gradient = ctx.createLinearGradient(10, 10, 10, 400);
-    my_gradient.addColorStop(0, "white");
-    my_gradient.addColorStop(0.5, "blue");
-    my_gradient.addColorStop(1, "brown");
-    ctx.fillStyle = my_gradient;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-
+﻿function reverseDirection(el) {
+    var elem = document.getElementById(el);
+    elem.style.animationDirection = "reverse";
+}
+function normalDirection(el) {
+    var elem = document.getElementById(el);
+    elem.style.animationDirection = "normal";
 }
